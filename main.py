751a0e3ba95a2fe1dc985241a3ca93339f046f46
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-01-05 14:52:36 +0200

    Add files via upload
    Now if user gives too short password, it will say it is too short, also if the sum of randomly generated amounts alphabets, numbers and symbols does not equal the minimum, it will generate the amounts again.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import string, secrets, random, getpass
+import sys, string, secrets, random, getpass
+
+#TODO getpass implementation
+#TODO automatic copy of password
 
 def random_number(start, end):
     if start >= end:
@@ -10,45 +13,46 @@ def alphabet_generator(count_alphabet):
     lower = list(string.ascii_lowercase)
     alphabet_list = upper + lower
     password_alphabets = [secrets.choice(alphabet_list) for i in range(count_alphabet)]
-    #print(password_alphabets)
     return password_alphabets
 
 def number_generator(count_numbers):
     number_list = list(string.digits)
     password_numbers = [secrets.choice(number_list) for i in range(count_numbers)]
-    #print(password_numbers)
     return password_numbers
 def symbol_generator(count_symbols):
     symbol_list = list(string.punctuation)
     password_symbols = [secrets.choice(symbol_list) for i in range(count_symbols)]
-    #print(password_symbols)
     return password_symbols
 def password_generator(count_alphabet, count_numbers, count_symbols):
     password_alphbets = alphabet_generator(count_alphabet)
     password_numbers = number_generator(count_numbers)
     password_symbols = symbol_generator(count_symbols)
     password_list = password_alphbets + password_numbers + password_symbols
-
     random.shuffle(password_list)
-    #print(password_list)
-    return password_list
+    password = ''.join(password_list)
+    return password
 
+def generate_random_numbers():
+    count_alphabet = random_number(1, 10)
+    count_numbers = random_number(1, 10)
+    count_symbols = random_number(1, 10)
+    return count_alphabet, count_numbers, count_symbols
 def main():
+    minimum = 12
     if input("Would you like custom password (y/n)?").lower() == 'y':
         count_alphabet = int(input("Give the amount of letters"))
         count_numbers = int(input("Give the amount of numbers"))
         count_symbols = int(input("Give the amount of symbols"))
+        if count_alphabet + count_numbers + count_symbols < minimum:
+            print("Too short password. Minimum length is:", minimum)
+            sys.exit()
     else:
-        count_alphabet = random_number(1, 10)
-        count_numbers = random_number(1, 10)
-        count_symbols = random_number(1, 10)
-        #return count_alphabet, count_numbers, count_symbols
-    password_list = password_generator(count_alphabet, count_numbers, count_symbols)
-    password = ''.join(password_list)
+            count_alphabet, count_numbers, count_symbols = generate_random_numbers()
+    password = password_generator(count_alphabet, count_numbers, count_symbols)
 
-    minimum = 12
     while len(password) < minimum:
-        print("")
+        #print("Too short generating again")
+        count_alphabet, count_numbers, count_symbols = generate_random_numbers()
         password_list = password_generator(count_alphabet, count_numbers, count_symbols)
         password = ''.join(password_list)
 
